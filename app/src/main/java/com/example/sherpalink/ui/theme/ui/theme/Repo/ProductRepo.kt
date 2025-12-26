57bb9e4041a1ba3cd590ce40f12b4dedb1c9d5bc
Saksham Ban <sakshamban@gmail.com>
2025-12-26 18:51:14 +0545

    class updates

---
+++ app/src/main/java/com/example/sherpalink/ui/theme/ui/theme/Repo/ProductRepo.kt
@@ -0,0 +1,25 @@
+package com.example.sherpalink
+
+data class UserModel(
+    val userId:String = "",
+    val firstName:String = "",
+    val lastName:String = "",
+    val email:String = "",
+    val dob:String = "",
+    val gender:String = "",
+    val role:String = ""   // "guide" or "user"
+){
+    fun toMap() : Map<String,Any?>{
+        return mapOf(
+            "userId" to userId,
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "email" to email,
+            "dob" to dob,
+            "gender" to gender,
+            "role" to role
+        )
+    }
+}
+
+
