8e51c12cbe32790a98bc4db8d35e1cda74e43b26
Yaseen Giri <yaseengiri77@uniglobecollege.edu.np>
2025-12-29 13:05:33 +0545

    error fix

--- app/src/main/java/com/example/sherpalink/SignInActivity.kt
+++ app/src/main/java/com/example/sherpalink/SignInActivity.kt
@@ -5,23 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.sherpalink.ui.auth.SignInScreen
 
 class SignInActivity : ComponentActivity() {
@@ -29,10 +12,15 @@ class SignInActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SignInScreen {
-                startActivity(Intent(this, DashboardActivity::class.java))
-                finish()
-            }
+            SignInScreen(
+                onSignInClick = {
+                    startActivity(Intent(this, DashboardActivity::class.java))
+                    finish()
+                },
+                onSignUpClick = {
+                    startActivity(Intent(this, SignUpActivity::class.java))
+                }
+            )
         }
     }
 }
