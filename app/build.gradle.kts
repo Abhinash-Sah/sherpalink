a82d7b430f445008b0b89717b208936df837f416
Yaseen Giri <yaseengiri77@uniglobecollege.edu.np>
2026-01-28 20:59:36 +0545

    gradle changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+
     implementation("com.google.maps.android:maps-compose:4.3.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
